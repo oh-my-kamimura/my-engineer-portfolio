bab59db7064b7f5fb5589ff508b5d1a42cd4fb24
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-11-21 01:51:35 +0900

    add: トップ画面を追加

---
+++ src/app/components/Top.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "../../styles/Top.module.scss"
+import ScrollAnimation from './ScrollAnimation';
+
+export default function Top() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.titleName}>
+				YOHEI KAMIMURA
+			</div>
+			<div className={styles.titleSite}>
+				&lt;engineer portfolio&gt;
+			</div>
+			<div className={styles.scroll}>
+				<ScrollAnimation/>
+			</div>
+			<Image
+				className={styles.mainImage}
+				src="/top.png"
+				alt="プロフィール画像"
+				fill
+			/>
+		</div>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import styles from "../styles/page.module.scss";
 import Header from "./components/Header";
+import Top from "./components/Top";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header/>
+      <div className={styles.top}>
+        <Top/>
+      </div>
     </div>
   );
 }
