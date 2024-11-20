f8382833480a7e974e92b9f9f26e2e4eb5f46397
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-11-20 22:29:26 +0900

    add: フォントを変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const jetBrainsMono = localFont({
+  src: "./fonts/JetBrainsMono-VariableFont_wght.ttf",
+  variable: "--font-jet-brains-mono",
   weight: "100 900",
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${jetBrainsMono.variable}`}>
         {children}
       </body>
     </html>
